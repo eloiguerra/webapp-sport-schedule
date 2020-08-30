188bca54a794bfd354b7e8f2172f1e8046e78403
eloiguerra <joseeloi.juninho@gmail.com>
2020-08-30 13:01:35 -0300

    Add navbar home

---
+++ src/components/NavBars/NavBarHome/index.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import Container from './styles';
+
+import NavLink from '../../NavLink';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+  faBell,
+  faCog,
+  faUser,
+  faDoorOpen
+} from '@fortawesome/free-solid-svg-icons';
+
+import logo from '../../../assets/images/miniLogo.png';
+
+
+export default function NavBarHome({logout}) {
+  const [visibleSideBar, setVisibleSideBar] = useState(false);
+  const [visibleDropdownConfig, setVisibleDropdownConfig] = useState(false);
+
+  const toggleSideBar = () => setVisibleSideBar(!visibleSideBar);
+  const toggleDropdownConfig = () => setVisibleDropdownConfig(!visibleDropdownConfig);
+
+  return (
+    <Container
+      visible = {visibleSideBar}
+      visibleDropdownConfig = {visibleDropdownConfig}
+    >
+      <div className = "top-navbar">
+        <div onClick = {toggleSideBar} className = "hamburguer">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className = "top-menu">
+          <div className = "logo-container">
+            <img className = "logo" src = {logo} alt = "" />
+            <p>SportSchedule</p>
+          </div>
+          <ul>
+            <li> <FontAwesomeIcon icon = {faSearch} /> </li>
+            <li> <FontAwesomeIcon icon = {faBell} /> </li>
+            <li onClick = {toggleDropdownConfig}>
+              <FontAwesomeIcon icon = {faCog} />
+               <ul className = "dropdown-config">
+                 <li>
+                  <NavLink
+                    color = "white" text = "Meu perfil"
+                    path = "/users" icon = {faUser}
+                  />
+                </li>
+                 <li onClick = {logout}>
+                  <FontAwesomeIcon icon = {faDoorOpen} /> Sair
+                </li>
+               </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className = "sidebar">
+        <ul>
+          <li>
+            <span className = "icon"> <FontAwesomeIcon icon = {faUser} /> </span>
+            <span className = "title"> Meu perfil </span>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/NavBars/NavBarHome/styles.js
@@ -0,0 +1,143 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  .top-navbar{
+    width: 100vw;
+    height: 60px;
+    display: flex;
+    position: fixed;
+    top: 0;
+
+    .top-menu{
+      width: calc(100% - 80px);
+      background: var(--color-white);
+      height: 100%;
+      border-top-right-radius: 20px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 20px;
+      box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+
+      .logo-container{
+        display: flex;
+        align-items: center;
+
+        letter-spacing: 1px;
+        font: 700 2.5rem Lora;
+
+        .logo{
+          height: 65px;
+        }
+      }
+
+      ul{
+        display: flex;
+
+        li{
+          position: relative;
+          display: block;
+          margin: 0 10px;
+          width: 35px;
+          height: 35px;
+          line-height: 35px;
+          border: 1px solid;
+          border-color: var(--color-secondary);
+          border-radius: 50%;
+          text-align: center;
+          color: var(--color-secondary);
+          cursor: pointer;
+          transition: .4s all;
+
+          ul{
+            position: absolute;
+            top: 45px;
+            right: 0;
+            width: 200px;
+            background: var(--color-secondary);
+            display: ${props => props.visibleDropdownConfig ? 'none' : 'flex'};
+            flex-direction: column;
+
+            li{
+              width: 100%;
+              color: var(--color-white);
+              border: unset;
+              margin: 0;
+              border-radius: 0;
+            }
+          }
+
+          &:hover{
+            color: var(--color-white);
+            background: var(--color-secondary);
+            border: 1px solid white;
+          }
+        }
+      }
+    }
+
+    .hamburguer{
+      width: 70px;
+      height: 100%;
+      background: var(--color-secondary);
+      padding: 15px 17px;
+      border-top-left-radius: 20px;
+      cursor: pointer;
+
+      div{
+        width: 35px;
+        height: 4px;
+        background: var(--color-white);
+        margin: 5px 0;
+        border-radius: 5px;
+      }
+    }
+  }
+
+  .sidebar{
+    position: fixed;
+    top: 60px;
+    left: 0;
+    background: var(--color-secondary);
+    width: ${props => props.visible ? '70px' : '200px'};
+    height: calc(100%);
+    border-bottom-left-radius: 20px;
+    transition: all .3s ease;
+
+    li{
+      display: block;
+      padding: 20px;
+      color: var(--color-white);
+      position: relative;
+      margin-bottom: 1px;
+      white-space: nowrap;
+
+      cursor: pointer;
+      &:hover{
+        background: var(--color-tertiary);
+      }
+
+      &::before{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 3px;
+        height: 100%;
+        background: var(--color-tertiary);
+      }
+
+      .icon{
+        margin-right: ${props => props.visible ? '0px' : '10px'};
+        display: ${props => props.visible ? 'flex' : 'inline-block'};
+        justify-content: ${props => props.visible && 'center'};
+      }
+
+      .title{
+        display: ${props => props.visible ? 'none' : 'inline-block'};
+      }
+    }
+  }
+`;
+
+export default Container;
