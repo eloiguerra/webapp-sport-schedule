054ea9ee9d1635e247e297a4f5499b5a6eef4721
eloiguerra <joseeloi.juninho@gmail.com>
2020-09-26 12:22:08 -0300

    style to visit profile page

---
+++ src/pages/VisitProfile/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  width: calc(100% - 70px);
+  margin-top: 70px;
+
+  @media screen and (min-width: 701px){
+    margin-left: 70px;
+  }
+
+  @media screen and (max-width: 700px){
+    width: 100%;
+  }
+`;
