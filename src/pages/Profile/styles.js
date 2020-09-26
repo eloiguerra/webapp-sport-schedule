fb7511076ad63b60e3bf9691337939653c83adf7
eloiguerra <joseeloi.juninho@gmail.com>
2020-09-26 12:21:42 -0300

    changing media query breakpoints

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -4,11 +4,11 @@ const Container = styled.div`
   width: calc(100% - 70px);
   margin-top: 70px;
 
-  @media screen and (min-width: 601px){
+  @media screen and (min-width: 701px){
     margin-left: 70px;
   }
 
-  @media screen and (max-width: 600px){
+  @media screen and (max-width: 700px){
     width: 100%;
   }
 `;
