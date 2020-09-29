21f603692841238c5a57db531d054e6255f1a0f0
eloiguerra <joseeloi.juninho@gmail.com>
2020-09-29 10:56:50 -0300

    Add card

---
+++ src/components/Card/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 90%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 16px;
+  background: var(--color-white-two);
+  -webkit-box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.75);
+  -moz-box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.75);
+  box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.75);
+`;
+
+
