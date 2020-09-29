df3ad2849f4fa58f64a2a31712912906c70a9553
eloiguerra <joseeloi.juninho@gmail.com>
2020-09-29 10:59:55 -0300

    Add more styles

--- src/pages/VisitProfile/styles.js
+++ src/pages/VisitProfile/styles.js
@@ -4,11 +4,66 @@ export const Container = styled.section`
   width: calc(100% - 70px);
   margin-top: 70px;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   @media screen and (min-width: 701px){
     margin-left: 70px;
   }
 
   @media screen and (max-width: 700px){
     width: 100%;
+  }
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: var(--color-white);
+
+  border-bottom: 2px solid var(--color-white-two);
+
+  margin-bottom: 16px;
+  .profile-photo{
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+  }
+
+  hr{
+    margin-top: 8px;
+    margin-bottom: 8px;
+    color: var(--color-gray);
+    width: 90%;
+  }
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  max-width: 150px;
+
+  border: none;
+  border-radius: 8px;
+  outline: none;
+
+  padding: 16px;
+
+  color: var(--color-white);
+  background: var(--color-primary);
+
+  cursor: pointer;
+
+  transition: all .3s;
+
+  svg{
+    margin-right: 8px;
+  }
+
+  &:hover{
+    transform: scale(0.95);
   }
 `;
