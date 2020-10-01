6a8399a1292e7f64ad265318b7a0437833df8368
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-01 10:19:33 -0300

    Add responsivity

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,9 +3,18 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: calc(100% - 70px);
   margin-top: 70px;
-  margin-left: 70px;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
+  @media screen and (min-width: 701px){
+    margin-left: 70px;
+  }
+
+  @media screen and (max-width: 700px){
+    width: 100%;
+  }
 `;
 
 export default Container;
