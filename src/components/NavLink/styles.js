34ffa617070ad1f601eaaff96e9ad05a7e908335
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-12 14:33:19 -0300

    margin choose

--- src/components/NavLink/styles.js
+++ src/components/NavLink/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
 export const StyledNavLink = styled(Link)`
-  margin-top: 16px;
+  margin-top: ${props => props.margin ? '16px' : '0'};
   font-size: ${props => props.color === "black" ? '1.5rem' : '1.8rem'};
   color: ${props => props.color === "black" ? 'var(--color-black)' : 'var(--color-white)'};
   &:hover{
