4339c88538d687fcaec71b4a56ce8019ed1f09af
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-12 14:32:03 -0300

    Trying esc press to close modal

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import Container, {CloseButton, Content} from './styles'
 
 export default function Modal({children, id = "modal", onClose = () => {}}) {
-  const avoidModal = (e) => e.target.id === id && onClose();
+  const avoidModal = e => e.target.id === id && onClose();
+
   return (
-    <Container id = {id} onClick = {(e) => avoidModal(e)}>
+    <Container
+      id = {id}
+      onClick = {e => avoidModal(e)}
+      onKeyDown = {e => e.key === "Escape" && onClose()}
+    >
       <Content>
         <CloseButton onClick = {onClose}> x </CloseButton>
         {children}
