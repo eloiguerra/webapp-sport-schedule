92fe279e41b31a8f8eeab3a26af631f8279a8d24
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-12 14:33:05 -0300

    Add margin choose

--- src/components/NavLink/index.js
+++ src/components/NavLink/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {StyledNavLink} from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function NavLink({text, icon, path, color = "black"}) {
+export default function NavLink({text, icon, path, color = "black", margin = 0}) {
   return (
-    <StyledNavLink color = {color} to = {path}>
+    <StyledNavLink margin = {margin} color = {color} to = {path}>
       {icon && <FontAwesomeIcon icon = {icon} />} {text}
     </StyledNavLink>
   )
