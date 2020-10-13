c0af2dc131214f5c39b863b7eb555d920938cc3b
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-13 10:23:38 -0300

    Add event to listener scroll

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,11 +1,20 @@
 import React,{useState, useEffect} from 'react';
 
-import { Container } from './styles';
+import {Container} from './styles';
 
 import api from '../../services/api';
 
 export default function Feed() {
   const [publications, setPublications] = useState([]);
+
+  window.addEventListener('scroll', () => {
+    const {clientHeight, scrollHeight, scrollTop} = document.documentElement;
+    const isPageBottomAlmostReached = scrollTop + clientHeight >= scrollHeight - 10;
+
+    if(isPageBottomAlmostReached){
+      console.log('calma ae meu camarada tá acabando a página');
+    }
+  })
 
   useEffect(() => {
     api.get('/friendsPublications')
