5eaad682c6eb31dbcef6e32b8121b9992ee60e64
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-18 17:43:13 -0300

    Add feed styles

--- src/components/Feed/styles.js
+++ src/components/Feed/styles.js
@@ -6,18 +6,58 @@ export const Container = styled.div`
   align-items: center;
 `;
 
+export const PublicationWrapper = styled.div`
+  margin-top: 16px;
+  padding: 8px;
+
+  width: 95%;
+  max-width: 600px;
+  min-width: 300px;
+
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 8px;
+  background: var(--color-white);
+  -webkit-box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.75);
+  -moz-box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.75);
+  box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.75);
+`;
+
 export const Header = styled.div`
   width: 100%;
 
-  img{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .user-info{
+    display: flex;
+    align-items: center;
+
+    img{
     width: 40px;
     height: 40px;
+
+    border-radius: 50%;
+    }
+
+    p{
+      font-weight: 700;
+
+      margin-left: 8px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  p{
+    font-size: 1.6rem;
+    margin: 4px 0;
   }
 
-  p{
-    font-size: 1.5rem;
-    font-weight: 700;
-
-    margin-left: 8px;
+  img{
+    width: 100%;
+    object-fit: cover;
   }
 `;
