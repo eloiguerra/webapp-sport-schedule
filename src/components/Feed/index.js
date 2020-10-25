9cb49c53f9e91b916bc6d19279626c4d46b2b2b4
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-25 13:47:08 -0300

    Add comment functions

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -2,21 +2,59 @@ import React,{useState, useEffect} from 'react';
 
 import api from '../../services/api';
 import useInfiniteScroll from '../../hooks/useInfiniteScroll';
+import useForm from '../../hooks/useForm';
 
 import DotLoader from '../Loaders/DotLoader';
 import {
   Container, PublicationWrapper,
-  Header, Content
+  Header, Content,
+  ToolBar, Comments
 } from './styles';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+  faThumbsUp,
+  faCommentAlt
+} from '@fortawesome/free-solid-svg-icons';
+import sendButton from '../../assets/images/send-button.svg';
 
-export default function Feed() {
+export default function Feed({user}) {
   const [publications, setPublications] = useState([]);
   const [paginationIndex, setPaginationIndex] = useState(1);
   const [loading, setLoading] = useInfiniteScroll(callback);
+  const [{values}, handleChange, handleSubmit] = useForm();
 
   function callback(){
     setPaginationIndex(paginationIndex + 1);
     setLoading(false);
+  }
+
+  const handleCommentSubmit = e => {
+    e.preventDefault();
+
+    const {comment} = values;
+    const publication = e.target.id;
+
+    if(comment){
+      api.post('/publications/comments', {
+        comment,
+        publication
+      })
+      .then(response => {
+        const newComment = publications.map(item => {
+          if(item._id === publication){
+            return response.data;
+          }
+          else{
+            return item;
+          }
+        });
+
+        setPublications(newComment);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
   }
 
   useEffect(() => {
@@ -41,6 +79,47 @@ export default function Feed() {
             {item.description && <p>{item.description}</p>}
             {item.image && <img src = {item.image.url} alt = "" />}
           </Content>
+          <ToolBar>
+            <div className = "container-reactions">
+              <button className = "toggle-reactions">
+                <FontAwesomeIcon icon = {faThumbsUp} />
+                Curtir
+              </button>
+              <div className = "reactions"> </div>
+            </div>
+            <button>
+              <FontAwesomeIcon icon = {faCommentAlt}/>
+              Comentar
+            </button>
+          </ToolBar>
+          <Comments>
+            <form
+                className = "post-comment"
+                onSubmit = {(e) => handleCommentSubmit(e)}
+                id = {item._id}
+            >
+              <img src = {user.profile_photo.url} alt = "" />
+              <textarea
+                name = "comment"
+                placeholder = "Comentar..."
+                onChange = {handleChange}
+              />
+              <button type = "submit">
+                <img src = {sendButton} alt = ""/>
+              </button>
+            </form>
+            {item.comments.map(comment => (
+              <div key = {comment._id} className = "user-comment">
+                <div className = "user">
+                 <img src = {comment.owner.profile_photo.url} alt = ""/>
+                 <p>{comment.owner.full_name}</p>
+                </div>
+                <p className = "comment">
+                  {comment.description}
+                </p>
+              </div>
+            ))}
+          </Comments>
         </PublicationWrapper>
       ))}
     </Container>
