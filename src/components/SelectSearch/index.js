38010fb19d5bf9737aa8eb6a526671df4d6b58f1
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-27 17:25:56 -0300

    Add select with filter

---
+++ src/components/SelectSearch/index.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react';
+import {
+  SelectBox, OptionsContainer,
+  Option, SearchBox,
+  SelectedContainer
+} from './styles';
+
+export default function SelectSearch({data, change}) {
+  const [values, setValues] = useState(data);
+  const [toggleOptions, setToggleOptions] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const findInSelect = e => {
+    const name = e.target.value;
+    console.log(name);
+
+    if(name !== ''){
+      setValues(prevValues =>
+        prevValues.filter(item => item.name.includes(name))
+      );
+    }
+    else{
+      setValues(data);
+    }
+  }
+
+  return (
+      <SelectBox>
+        <SelectedContainer
+          onClick = {() => setToggleOptions(!toggleOptions)}
+        >
+          {selected ? selected.name : 'Selecione um esporte'}
+        </SelectedContainer>
+        <OptionsContainer options = {toggleOptions}>
+          <SearchBox>
+            <input
+              type = "text"
+              name = "select-search"
+              placeholder = "Jogo ou esporte"
+              autoComplete = "off"
+              onChange = {e => findInSelect(e)}
+            />
+          </SearchBox>
+
+          {values.map(item => (
+            <Option
+              key = {item._id}
+              onClick = {() => {
+                setSelected({
+                  id: item._id,
+                  name: item.name
+                })
+                setToggleOptions(false);
+              }}
+            >
+              <input
+                type = "radio"
+                id = {item._id}
+                name = "data"
+                value = {item.name}
+                onChange = {change}
+              />
+              <label htmlFor = {item._id}> {item.name} </label>
+            </Option>
+          ))}
+        </OptionsContainer>
+      </SelectBox>
+  )
+}
